099fd47b225ff040c4444f01da72b4f038460028
Andres Eusse <carloseusse@gmail.com>
2017-02-27 08:50:08 -0500

    Added menu functionality

--- src/states/Title.js
+++ src/states/Title.js
@@ -10,8 +10,13 @@ export default class Title extends Phaser.State {
   create() {
     const {centerX: x, centerY: y} = this.world;
     var style = { font: '32px Arial', fill: '#FFF', wordWrap: true, align: 'center' };
-    var text = this.game.add.text(x, y, 'Title', style);
+    var text = this.game.add.text(x, y-30, 'Title', style);
     text.anchor.set(0.5);
+    var loginLabel = this.game.add.text(x, y + 40, 'Login', { font: '24px Arial', fill: '#fff', align: 'center' });
+    loginLabel.inputEnabled = true;
+    loginLabel.events.onInputUp.add(function () {
+      console.log('Login Clicked');
+    });
   }
 
   update() {
